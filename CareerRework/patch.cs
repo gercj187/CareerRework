5cd9eaca22b48863ada9829c00351768745ae898
CJ187 <mail@cj187.de>
2025-04-29 22:40:24 +0200

    Add files via upload

--- CareerRework/patch.cs
+++ CareerRework/patch.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
+using System.Reflection;
 using DV;
 using DV.Common;
 using DV.Booklets;
@@ -13,7 +14,9 @@ using DV.UserManagement;
 using DV.Utils;
 using HarmonyLib;
 using UnityModManagerNet;
-using Debug = UnityEngine.Debug; // Explicitly use UnityEngine.Debug
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
 
 
 namespace CareerRework
@@ -24,26 +27,26 @@ namespace CareerRework
 		// Run patch after super method
 		static void Postfix(LicenseManager __instance)
 		{
-			// Job Licenses Prices
-			JobLicenses.Shunting.ToV2().price = 15000f;         //	15000f;
-			JobLicenses.LogisticalHaul.ToV2().price = 25000f;   //	25000f;
-			JobLicenses.FreightHaul.ToV2().price = 50000f;      //	50000f;
-			JobLicenses.TrainLength1.ToV2().price = 25000f;     //	25000f;
-			JobLicenses.TrainLength2.ToV2().price = 50000f;     //	50000f;
-			JobLicenses.Fragile.ToV2().price = 75000f;          //	75000f;
-			JobLicenses.Hazmat1.ToV2().price = 100000f;         //	100000f;
-			JobLicenses.Hazmat2.ToV2().price = 200000f;         //	200000f;
-			JobLicenses.Hazmat3.ToV2().price = 400000f;         //	400000f;
-			JobLicenses.Military1.ToV2().price = 250000f;       //	250000f;
-			JobLicenses.Military2.ToV2().price = 500000f;       //	500000f;
-			JobLicenses.Military3.ToV2().price = 1000000f;      //	1000000f;
+			// Job Licenses Prices									vanilla			modded
+			JobLicenses.Shunting.ToV2().price = 15000f;         //	1000			15000
+			JobLicenses.LogisticalHaul.ToV2().price = 25000f;   //	20000			25000
+			JobLicenses.FreightHaul.ToV2().price = 50000f;      //	OWNED			50000
+			JobLicenses.TrainLength1.ToV2().price = 25000f;     //	10000			25000
+			JobLicenses.TrainLength2.ToV2().price = 50000f;     //	20000			50000
+			JobLicenses.Fragile.ToV2().price = 75000f;          //	10000			75000
+			JobLicenses.Hazmat1.ToV2().price = 100000f;         //	40000			100000
+			JobLicenses.Hazmat2.ToV2().price = 200000f;         //	130000			200000
+			JobLicenses.Hazmat3.ToV2().price = 400000f;         //	290000			400000
+			JobLicenses.Military1.ToV2().price = 250000f;       //	100000			250000
+			JobLicenses.Military2.ToV2().price = 500000f;       //	30000			500000
+			JobLicenses.Military3.ToV2().price = 1000000f;      //	400000			1000000
 
 			// Job Licenses Conditions
 			JobLicenses.Shunting.ToV2().requiredGeneralLicense = GeneralLicenseType.TrainDriver.ToV2();
 			JobLicenses.LogisticalHaul.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
 			JobLicenses.LogisticalHaul.ToV2().requiredJobLicense = JobLicenses.Shunting.ToV2();
 			JobLicenses.FreightHaul.ToV2().requiredJobLicense = JobLicenses.LogisticalHaul.ToV2();
-			JobLicenses.TrainLength1.ToV2().requiredJobLicense = JobLicenses.FreightHaul.ToV2();
+			JobLicenses.TrainLength1.ToV2().requiredJobLicense = JobLicenses.LogisticalHaul.ToV2();
 			JobLicenses.TrainLength2.ToV2().requiredJobLicense = JobLicenses.TrainLength1.ToV2();
 			JobLicenses.Fragile.ToV2().requiredJobLicense = JobLicenses.FreightHaul.ToV2();
 			JobLicenses.Hazmat1.ToV2().requiredJobLicense = JobLicenses.Fragile.ToV2();
@@ -53,39 +56,41 @@ namespace CareerRework
 			JobLicenses.Military2.ToV2().requiredJobLicense = JobLicenses.Military1.ToV2();
 			JobLicenses.Military3.ToV2().requiredJobLicense = JobLicenses.Military2.ToV2();
 
-			// Career Licenses Prices
-			GeneralLicenseType.TrainDriver.ToV2().price = 150000f;      //	150000f;
-			GeneralLicenseType.ManualService.ToV2().price = 25000f;     //	25000f;
-			GeneralLicenseType.ConcurrentJobs1.ToV2().price = 25000f;   //	25000f;
-			GeneralLicenseType.ConcurrentJobs2.ToV2().price = 50000f;   //	50000f;
-			GeneralLicenseType.MultipleUnit.ToV2().price = 50000f;      //	50000f;
-			GeneralLicenseType.Dispatcher1.ToV2().price = 50000f;       //	50000f;
-			GeneralLicenseType.MuseumCitySouth.ToV2().price = 150000f;  //	150000f;
+			// Career Licenses Prices										vanilla			modded
+			GeneralLicenseType.TrainDriver.ToV2().price = 150000f;      //	OWNED			150000
+			GeneralLicenseType.ManualService.ToV2().price = 25000f;     //	20000			25000
+			GeneralLicenseType.ConcurrentJobs1.ToV2().price = 25000f;   //	10000			25000
+			GeneralLicenseType.ConcurrentJobs2.ToV2().price = 50000f;   //	20000			50000
+			GeneralLicenseType.MultipleUnit.ToV2().price = 50000f;      //	30000			50000
+			GeneralLicenseType.Dispatcher1.ToV2().price = 50000f;       //	10000			50000
+			GeneralLicenseType.MuseumCitySouth.ToV2().price = 150000f;  //	15000			150000
 
 			// Career Licenses Conditions
 			GeneralLicenseType.ManualService.ToV2().requiredJobLicense = JobLicenses.Basic.ToV2();
+			GeneralLicenseType.ManualService.ToV2().requiredGeneralLicense = GeneralLicenseType.Dispatcher1.ToV2();
 			GeneralLicenseType.ConcurrentJobs1.ToV2().requiredJobLicense = JobLicenses.LogisticalHaul.ToV2();
 			GeneralLicenseType.ConcurrentJobs2.ToV2().requiredGeneralLicense = GeneralLicenseType.ConcurrentJobs1.ToV2();
 			GeneralLicenseType.MultipleUnit.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
 			GeneralLicenseType.MultipleUnit.ToV2().requiredJobLicense = JobLicenses.TrainLength2.ToV2();
-			GeneralLicenseType.Dispatcher1.ToV2().requiredGeneralLicense = GeneralLicenseType.ConcurrentJobs2.ToV2();
+			GeneralLicenseType.Dispatcher1.ToV2().requiredGeneralLicense = GeneralLicenseType.ConcurrentJobs1.ToV2();
 			GeneralLicenseType.MuseumCitySouth.ToV2().requiredGeneralLicense = GeneralLicenseType.ManualService.ToV2();
 
-			// Loco Licenses Prices
-			GeneralLicenseType.DE2.ToV2().price = 100000f;      //	150000f;
-			GeneralLicenseType.DM3.ToV2().price = 100000f;      //	150000f;
-			GeneralLicenseType.DH4.ToV2().price = 400000f;      //	400000f;
-			GeneralLicenseType.DE6.ToV2().price = 1000000f;     //	1000000f;
-			GeneralLicenseType.S060.ToV2().price = 250000f;     //	250000f;
-			GeneralLicenseType.SH282.ToV2().price = 750000f;    //	750000f;
+			// Loco Licenses Prices									vanilla			modded
+			GeneralLicenseType.DE2.ToV2().price = 100000f;      //	OWNED			150000
+			GeneralLicenseType.DM3.ToV2().price = 100000f;      //	30000			150000
+			GeneralLicenseType.S060.ToV2().price = 250000f;     //	20000			250000
+			GeneralLicenseType.DH4.ToV2().price = 400000f;      //	50000			400000
+			GeneralLicenseType.SH282.ToV2().price = 750000f;    //	50000			750000
+			GeneralLicenseType.DE6.ToV2().price = 1000000f;     //	200000			1000000
 
 			// Loco Licenses Conditions
-			GeneralLicenseType.DE2.ToV2().requiredJobLicense = JobLicenses.Basic.ToV2();
-			GeneralLicenseType.DE2.ToV2().requiredGeneralLicense = GeneralLicenseType.TrainDriver.ToV2();
-			GeneralLicenseType.DM3.ToV2().requiredJobLicense = JobLicenses.Basic.ToV2();
-			GeneralLicenseType.DM3.ToV2().requiredGeneralLicense = GeneralLicenseType.TrainDriver.ToV2();
+			GeneralLicenseType.DE2.ToV2().requiredJobLicense = JobLicenses.Shunting.ToV2();
+			GeneralLicenseType.DE2.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
+			GeneralLicenseType.DM3.ToV2().requiredJobLicense = JobLicenses.Shunting.ToV2();
+			GeneralLicenseType.DM3.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
 			GeneralLicenseType.DH4.ToV2().requiredJobLicense = JobLicenses.TrainLength1.ToV2();
-			GeneralLicenseType.DE6.ToV2().requiredGeneralLicense = GeneralLicenseType.ConcurrentJobs2.ToV2();
+			GeneralLicenseType.DE6.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
+			GeneralLicenseType.DE6.ToV2().requiredJobLicense = JobLicenses.TrainLength2.ToV2();
 			GeneralLicenseType.S060.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
 			GeneralLicenseType.S060.ToV2().requiredJobLicense = JobLicenses.LogisticalHaul.ToV2();
 			GeneralLicenseType.SH282.ToV2().requiredGeneralLicense = GeneralLicenseType.NotSet.ToV2();
@@ -93,68 +98,34 @@ namespace CareerRework
 		}
 
 		// Prefix: Wird vor der Originalfunktion aufgerufen
+		
+
 		static bool Prefix(SaveGameData data)
 		{
-			Debug.Log("[LicenseManagerMod] Loading GeneralLicenses, JobLicenses, and Garages...");
-
-			// Lade GeneralLicenses
-			ProcessListOfIDs(data.GetStringArray("Licenses_General"), Globals.G.Types.generalLicenses).ForEach(delegate (GeneralLicenseType_v2 l)
-			{
-				SingletonBehaviour<LicenseManager>.Instance.AcquireGeneralLicense(l);
-			});
-
-			// Lade JobLicenses
-			ProcessListOfIDs(data.GetStringArray("Licenses_Jobs"), Globals.G.Types.jobLicenses).ForEach(delegate (JobLicenseType_v2 l)
-			{
-				SingletonBehaviour<LicenseManager>.Instance.AcquireJobLicense(l);
-			});
-
-			// Lade Garages
-			ProcessListOfIDs(data.GetStringArray("Garages"), Globals.G.Types.garages).ForEach(delegate (GarageType_v2 g)
-			{
-				SingletonBehaviour<LicenseManager>.Instance.UnlockGarage(g);
-			});
-
-			Debug.Log("[LicenseManagerMod] All data loaded successfully.");
-
-			// Blockiere die Originalmethode vollständig
+			Debug.Log("[CareerRework] Loading GeneralLicenses, JobLicenses, and Garages...");
+			ProcessListOfIDs(data.GetStringArray("Licenses_General"), Globals.G.Types.generalLicenses).ForEach(l => SingletonBehaviour<LicenseManager>.Instance.AcquireGeneralLicense(l));
+			ProcessListOfIDs(data.GetStringArray("Licenses_Jobs"), Globals.G.Types.jobLicenses).ForEach(l => SingletonBehaviour<LicenseManager>.Instance.AcquireJobLicense(l));
+			ProcessListOfIDs(data.GetStringArray("Garages"), Globals.G.Types.garages).ForEach(g => SingletonBehaviour<LicenseManager>.Instance.UnlockGarage(g));
+			Debug.Log("[CareerRework] All data loaded successfully.");
 			return false;
 		}
 
 		// Hilfsmethode aus der Originalklasse extrahiert
 		private static List<T> ProcessListOfIDs<T>(IEnumerable<string> idList, IEnumerable<T> sourceList) where T : Thing_v2
 		{
-			if (idList == null)
-			{
-				return new List<T>();
-			}
+			if (idList == null) return new List<T>();
 			List<T> list = new List<T>();
 			foreach (string id in idList)
 			{
 				T val = sourceList.FirstOrDefault(t => t.id == id);
-				if (val != null)
-				{
-					list.Add(val);
-				}
-				else
-				{
-					Debug.LogError("Unknown thing (" + typeof(T).Name + ") ID in save file: " + id);
-				}
+				if (val != null) list.Add(val);
+				else Debug.LogError("Unknown thing (" + typeof(T).Name + ") ID in save file: " + id);
 			}
 			return list;
 		}
 	}
-
-	public static class LicenseManagerMod
-	{
-		public static void Init()
-		{
-			var harmony = new Harmony("com.yourname.licensemanagerpatch");
-			harmony.PatchAll();
-			Debug.Log("[LicenseManagerMod] Harmony patches applied.");
-		}
-	}
-
+	
+	// PATCH: StartGameData_NewCareer.PrepareNewSaveData
 	[HarmonyPatch(typeof(StartGameData_NewCareer))]
 	public static class StartGameData_NewCareerPatch
 	{
@@ -186,47 +157,123 @@ namespace CareerRework
 				Debug.LogError("Unexpected state: difficultyParams are null for new career session. Using default values in attempt to recover.");
 			}
 			session.PerformGameplayEntryDifficultyCheck(DifficultyToUse);
+
 			if (skipTutorial)
-			{	/*
-				foreach (GeneralLicenseType_v2 tutorialGeneralLicense in LicenseManager.TutorialGeneralLicenses)
-				{
-					SingletonBehaviour<UnlockablesManager>.Instance.UnlockThing(tutorialGeneralLicense);
-					saveGameData.AddToStringArray("Licenses_General", tutorialGeneralLicense.id, enforceUnique: true);
-				}*/
-				saveGameData.SetFloat("Player_money", 270000f);			// if the game will start complete without licenses
-				//saveGameData.SetFloat("Player_money", 17250f);		// if the game will start with Tutorial licenses (Driver and DE2)
-				saveGameData.SetBool("Tutorial_01_completed", value: true);
-				saveGameData.SetBool("Tutorial_02_completed", value: true);
-				saveGameData.SetBool("Tutorial_03_completed", value: true);
-				GameParams.StartingItemsType startingItems = Globals.G.GameParams.StartingItems;
-				switch (startingItems)
-				{
-					case GameParams.StartingItemsType.Basic:
-					case GameParams.StartingItemsType.Auto:
-						saveGameData.SetInt("Starting_items", 0);
-						break;
-					case GameParams.StartingItemsType.Expanded:
-						saveGameData.SetInt("Starting_items", 1);
-						break;
-					case GameParams.StartingItemsType.Engineer:
-						saveGameData.SetInt("Starting_items", 2);
-						break;
-					default:
-						Debug.LogError($"Unexpected state: Unhandled entry {startingItems}. Using basic starting items");
-						saveGameData.SetInt("Starting_items", 0);
-						break;
-				}
-				session.GameData.SetBool("Difficulty_picked", value: true);
+			{
+				saveGameData.SetFloat("Player_money", 270000f);
+				saveGameData.SetBool("Tutorial_01_completed", true);
+				saveGameData.SetBool("Tutorial_02_completed", true);
+				saveGameData.SetBool("Tutorial_03_completed", true);
+				saveGameData.SetInt("Starting_items", 0);
+				session.GameData.SetBool("Difficulty_picked", true);
 			}
 			else
 			{
-				saveGameData.SetBool("Tutorial_01_completed", value: false);
-				saveGameData.SetBool("Tutorial_02_completed", value: false);
-				saveGameData.SetBool("Tutorial_03_completed", value: false);
-				session.GameData.SetBool("Difficulty_picked", value: false);
-				saveGameData.SetFloat("Player_money", 0f);
+				saveGameData.SetFloat("Player_money", 270000f);
+				saveGameData.SetBool("Tutorial_01_completed", true);
+				saveGameData.SetBool("Tutorial_02_completed", true);
+				saveGameData.SetBool("Tutorial_03_completed", true);
+				saveGameData.SetInt("Starting_items", 0);
+				session.GameData.SetBool("Difficulty_picked", true);
 			}
-			return false; // Skip the original method
+
+			return false;
+		}
+	}
+
+	public static class LicenseManagerMod
+	{
+		public static void Init()
+		{
+			var harmony = new Harmony("com.yourname.licensemanagerpatch");
+			harmony.PatchAll();
+			Debug.Log("[CareerRework] Harmony patches applied.");
+
+			// entfernt – ersetzt durch Patch_WorldStreamingInit
+
+			var field = typeof(LicenseManager).GetField("TutorialGeneralLicenses", BindingFlags.Static | BindingFlags.Public);
+			if (field != null)
+			{
+				field.SetValue(null, new List<GeneralLicenseType_v2> { GeneralLicenseType.NotSet.ToV2() });
+				Debug.Log("[CareerRework] TutorialGeneralLicenses erfolgreich auf NotSet umgestellt.");
+			}
+			else
+			{
+				Debug.LogError("[CareerRework] Fehler: Feld TutorialGeneralLicenses nicht gefunden.");
+			}
+		}
+
+		public static IEnumerator BlockDE2Later()
+		{
+			yield return new WaitForSeconds(5f);
+
+			foreach (var loco in Object.FindObjectsOfType<TrainCar>())
+            {
+                 if (loco.carType == TrainCarType.LocoShunter)
+				{
+					CreateLocoZoneBlocker(loco);
+				}
+            }
+            Debug.Log("[CareerRework] Alle DE2-Loks mit ZoneBlocker versehen.");
+		}
+
+        public static void CreateLocoZoneBlocker(TrainCar loco)
+		{
+			GameObject blockerObject = new GameObject($"LocoZoneBlocker_{loco.ID}");
+			blockerObject.transform.SetParent(loco.interior);
+			blockerObject.transform.localPosition = Vector3.zero;
+			blockerObject.transform.localRotation = Quaternion.identity;
+						
+			GameObject blockedDE2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			blockedDE2.transform.SetParent(blockerObject.transform);
+			//blockedDE2.transform.localPosition = new Vector3(0f, 2f, 0f);
+			blockedDE2.transform.localPosition = new Vector3(0f, 1.5f, 0f);
+			blockedDE2.transform.localRotation = Quaternion.identity;
+			//blockedDE2.transform.localScale = new Vector3(3.2f, 4f, 7.6f);
+			blockedDE2.transform.localScale = new Vector3(3.5f, 5f, 7.8f);
+			blockedDE2.GetComponent<Renderer>().enabled = false;
+			
+			Collider col = blockedDE2.GetComponent<Collider>();
+			if (col != null)
+			{
+				col.isTrigger = true;
+			}
+
+			LocoZoneBlocker locoZoneBlocker = blockerObject.AddComponent<LocoZoneBlocker>();
+			locoZoneBlocker.blockerObjectsParent = blockerObject;
+			locoZoneBlocker.cab = loco.GetComponentInChildren<CabTeleportDestination>();
+			
+			Debug.Log($"[CareerRework] LocoZoneBlocker auf DE2 '{loco.ID}' erstellt.");
+		}
+	
+		public static IEnumerator MonitorAndBlockDE2s()
+		{
+			while (true)
+			{
+				foreach (var loco in Object.FindObjectsOfType<TrainCar>())
+				{
+					if (loco.carType == TrainCarType.LocoShunter)
+					{
+						// Falls kein Blocker dran ist
+						if (loco.transform.Find($"LocoZoneBlocker_{loco.ID}") == null)
+						{
+							CreateLocoZoneBlocker(loco);
+						}
+					}
+				}
+				yield return new WaitForSeconds(10f); // alle 10 Sekunden neu scannen
+			}
+		}
+    }
+
+	[HarmonyPatch(typeof(WorldStreamingInit), "Awake")]
+	public static class Patch_WorldStreamingInit
+	{
+		static void Postfix()
+		{
+			Debug.Log("[CareerRework] WorldStreamingInit gestartet – Starte BlockDE2Later()");
+			CoroutineManager.Instance.Run(LicenseManagerMod.BlockDE2Later());
+			CoroutineManager.Instance.Run(LicenseManagerMod.MonitorAndBlockDE2s());
 		}
 	}
 }
